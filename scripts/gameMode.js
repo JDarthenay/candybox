fa843250895405d53eb9d502581ff4afe5b857b8
Julien <julien.darthenay@free.fr>
2021-05-02 18:44:44 +0200

    New eXtended Game button

--- scripts/gameMode.js
+++ scripts/gameMode.js
@@ -1,13 +1,25 @@
 var gameMode = {
     setting: "normal",
+    eXtended: false,
+    unlockHiddenFeatures: false,
 
     setMode : function(mode){
         this.setting = mode;
         this.setText();
+        this.check();
     },
 
     setText : function() {
-        $("#game_mode").html("You are playing a " + this.setting + " game.");
+        if (this.setting == "eXtended") {
+            $("#game_mode").html("You are playing an " + this.setting + " game.");
+        } else {
+            $("#game_mode").html("You are playing a " + this.setting + " game.");
+        }
+    },
+    
+    check : function() {
+        this.eXtended = this.setting == "eXtended";
+        this.unlockHiddenFeatures = this.eXtended && (candies.nbrThrown >= 100);
     },
     
     onload : function() {
