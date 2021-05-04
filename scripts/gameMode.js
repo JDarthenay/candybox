dcd9c76da4809b37499fcf229ff26544ee298f63
Julien <julien.darthenay@free.fr>
2021-05-04 19:39:27 +0200

    Buy a set of scrolls

--- scripts/gameMode.js
+++ scripts/gameMode.js
@@ -17,9 +17,50 @@ var gameMode = {
         }
     },
     
+    updateScrollSetList : function() {
+        var list = htmlInteraction.getElement("scroll_set_type");
+        var nbScrollType = objects.list.magicianHat.have ? 5 : 3;
+        
+        while (list.options.length < nbScrollType) {
+            var option = document.createElement("option");
+            var optionText;
+            switch (list.options.length) {
+                case 0:
+                    optionText = potions.list.fireScroll.buttonText;
+                break;
+
+                case 1:
+                    optionText = potions.list.acidRainScroll.buttonText;
+                break;
+
+                case 2:
+                    optionText = potions.list.teleportScroll.buttonText;
+                break;
+
+                case 3:
+                    optionText = potions.list.impInvocationScroll.buttonText;
+                break;
+
+                case 4:
+                    optionText = potions.list.earthquakeScroll.buttonText;
+                break;
+
+            }
+            
+            if (optionText.endsWith(" scroll")) {
+                optionText = optionText.substring(0, optionText.length - 7);
+            }
+            
+            option.text = optionText;
+            list.options.add(option);
+        }
+    },
+    
     check : function() {
         this.eXtended = this.setting == "eXtended";
         this.unlockHiddenFeatures = this.eXtended && (candies.nbrThrown >= 100);
+        htmlInteraction.revealClass("extended_game_only", this.eXtended);
+        this.updateScrollSetList();
     },
     
     onload : function() {
